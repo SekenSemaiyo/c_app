98d8160a519f57c735793c1b4f689a3fb845f9d5
yshuichiro <shuichiro0911@gmail.com>
2024-09-17 16:37:28 +0900

    create logo

--- c_app/src/App.js
+++ c_app/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Icon from './Icon';
 
 function App() {
   return (
     <div className="App">
-
+      <Icon />
     </div>
   );
 }
-//遅くてごめんね
+
 export default App;
---
+++ c_app/src/Icon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from './images/human1.png';
+import './App.css';
+
+const Icon = () => {
+  return (
+    <div>
+        <img src={logo} alt="ロゴ" className="logo_layout"/>
+    </div>
+  )
+}
+
+export default Icon
