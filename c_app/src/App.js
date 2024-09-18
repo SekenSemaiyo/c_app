58ac53f135b5fa5da3bb19b359cea398dc56f630
Your Name <you@example.com>
2024-09-18 15:12:36 +0900

    supell

--- c_app/src/App.js
+++ c_app/src/App.js
@@ -3,14 +3,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TopPage from './Pages/TopPage';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
-// import AboutPage from './Pages/AbputPage'; // AboutPage コンポーネントをインポート
 import ListPage from './Pages/ListPage';
 import SendMoneyPage from './Pages/SendMoneyPage';
-import SendCompletionPage from './Pages/SendComplitionPage';
+import SendCompletionPage from './Pages/SendCompletionPage';
 import RequestPage from './Pages/RequestPage';
-import RequestComplition from './Pages/RequestComplitionPage';
+import RequestCompletion from './Pages/RequestCompletionPage';
 import PayPage from './Pages/PayPage';
-import PaymentComplition from './Pages/PaymentComplitionPage';
+import PaymentCompletion from './Pages/PaymentCompletionPage';
 import HistoryPage from './Pages/HistoryPage';
 
 function App() {
@@ -23,9 +22,9 @@ function App() {
           <Route path="/sendmoney" element={<SendMoneyPage />} /> {/* 送金処理画面 */}
           <Route path="/sendcompletion" element={<SendCompletionPage />} /> {/* 送金完了画面 */}
           <Route path="/request" element={<RequestPage />} /> {/* 請求画面 */}
-          <Route path="/request-completion" element={<RequestComplition />} /> {/* リンク作成完了画面 */}
+          <Route path="/request-completion" element={<RequestCompletion />} /> {/* リンク作成完了画面 */}
           <Route path="/pay" element={<PayPage/>} /> {/* 請求支払い画面*/}
-          <Route path="/payment-completion" element={<PaymentComplition/>} /> {/* 請求支払い完了画面*/}
+          <Route path="/payment-completion" element={<PaymentCompletion/>} /> {/* 請求支払い完了画面*/}
           <Route path="/history" element={<HistoryPage />} /> {/* 請求履歴確認画面 */}
         </Routes>
       </div>
