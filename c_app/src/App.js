6eb50627c03721bb9917ac11cb57467d08cafeec
yshuichiro <shuichiro0911@gmail.com>
2024-09-18 14:50:00 +0900

    add path

--- c_app/src/App.js
+++ c_app/src/App.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import AboutPage from './Pages/AbputPage'; // AboutPage コンポーネントをインポート
 import ListPage from './Pages/ListPage';
 import SendMoneyPage from './Pages/SendMoneyPage';
-import SendComplitionPage from './Pages/SendComplitionPage';
+import SendCompletionPage from './Pages/SendComplitionPage';
 import RequestPage from './Pages/RequestPage';
 import RequestComplition from './Pages/RequestComplitionPage';
 import PayPage from './Pages/PayPage';
@@ -19,13 +19,13 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<TopPage />} /> {/* トップページ */}
-          <Route path="/" element={<ListPage />} /> {/* 送金宛先一覧 */}
-          <Route path="/" element={<SendMoneyPage />} /> {/* 送金処理画面 */}
-          <Route path="/" element={<SendComplitionPage />} /> {/* 送金完了画面 */}
-          <Route path="/" element={<RequestPage />} /> {/* 請求画面 */}
-          <Route path="/" element={<RequestComplition />} /> {/* リンク作成完了画面 */}
-          <Route path="/" element={<PayPage/>} /> {/* 請求支払い画面*/}
-          <Route path="/" element={<PaymentComplition/>} /> {/* 請求支払い完了画面*/}
+          <Route path="/list" element={<ListPage />} /> {/* 送金宛先一覧 */}
+          <Route path="/sendmoney" element={<SendMoneyPage />} /> {/* 送金処理画面 */}
+          <Route path="/sendcompletion" element={<SendCompletionPage />} /> {/* 送金完了画面 */}
+          <Route path="/request" element={<RequestPage />} /> {/* 請求画面 */}
+          <Route path="/request-completion" element={<RequestComplition />} /> {/* リンク作成完了画面 */}
+          <Route path="/pay" element={<PayPage/>} /> {/* 請求支払い画面*/}
+          <Route path="/payment-completion" element={<PaymentComplition/>} /> {/* 請求支払い完了画面*/}
           <Route path="/" element={<HistoryPage />} /> {/* 請求履歴確認画面 */}
         </Routes>
       </div>
