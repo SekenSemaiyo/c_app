aaac41fc1275f8d071f481bd91d041520a1f8280
chacha3141 <s2420500@u.tsukuba.ac.jp>
2024-09-18 15:34:21 +0900

    Revert "Merge branch 'sagawa2' of https://github.com/SekenSemaiyo/c_app into sagawa2"
    This reverts commit e511d0aa7090024d6390cbbb1773c6c6d776a110, reversing
    changes made to 47bde0c8bdc00d7c2bd003cddd918621bb22297c.

--- c_app/src/App.js
+++ c_app/src/App.js
@@ -2,16 +2,7 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TopPage from './Pages/TopPage';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-// import AboutPage from './Pages/AbputPage'; // AboutPage コンポーネントをインポート
-import ListPage from './Pages/ListPage';
-import SendMoneyPage from './Pages/SendMoneyPage';
-import SendCompletionPage from './Pages/SendComplitionPage';
-import RequestPage from './Pages/RequestPage';
-import RequestComplition from './Pages/RequestComplitionPage';
-import PayPage from './Pages/PayPage';
-import PaymentComplition from './Pages/PaymentComplitionPage';
-import HistoryPage from './Pages/HistoryPage';
+import SendComplitionPage from './Pages/SendComplitionPage'; // SendComplitionPage コンポーネントをインポート
 
 function App() {
   return (
@@ -19,14 +10,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<TopPage />} /> {/* トップページ */}
-          <Route path="/list" element={<ListPage />} /> {/* 送金宛先一覧 */}
-          <Route path="/sendmoney" element={<SendMoneyPage />} /> {/* 送金処理画面 */}
-          <Route path="/sendcompletion" element={<SendCompletionPage />} /> {/* 送金完了画面 */}
-          <Route path="/request" element={<RequestPage />} /> {/* 請求画面 */}
-          <Route path="/request-completion" element={<RequestComplition />} /> {/* リンク作成完了画面 */}
-          <Route path="/pay" element={<PayPage/>} /> {/* 請求支払い画面*/}
-          <Route path="/payment-completion" element={<PaymentComplition/>} /> {/* 請求支払い完了画面*/}
-          <Route path="/" element={<HistoryPage />} /> {/* 請求履歴確認画面 */}
+          <Route path="/about" element={<SendComplitionPage />} /> {/* アバウトページ */}
         </Routes>
       </div>
     </Router>
